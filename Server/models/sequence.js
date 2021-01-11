049c6cecc965c93586fe1d7e2373cc90b900d6a4
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-01-11 23:51:34 +0800

    adding core code in backend

--- Server/models/sequence.js
+++ Server/models/sequence.js
@@ -1,22 +1,26 @@
 import mongoose from 'mongoose';
+
 const Schema = mongoose.Schema;
 
-const CounterSchema = Schema({
-    _id: { type: String, required: true},
-    seq: { type: Number, default: 0},
+const CounterSchema = new Schema({
+    _id: {type: String, required: true},
+    seq: {type: Number, default: 0},
 });
 
 export const counter = mongoose.model('counter', CounterSchema);
 
-export const Sequence = function() {
+export const Sequence = function () {
     const user_counter = () => {
         return new Promise((resolve, reject) => {
             counter.collection.insert({
                 _id: "user_counter",
                 seq: 1
             }, function (err) {
-                if (err) console.log(err)
-                setTimeout(resolve, 10)
+                if (err) {
+                    console.log(err)
+                } else {
+                    setTimeout(resolve, 10)
+                }
             })
         })
     }
@@ -27,8 +31,11 @@ export const Sequence = function() {
                 _id: "comment_counter",
                 seq: 1,
             }, function (err) {
-                if (err) console.log(err)
-                setTimeout(resolve, 10)
+                if (err) {
+                    console.log(err)
+                } else {
+                    setTimeout(resolve, 10)
+                }
             })
         })
     };
